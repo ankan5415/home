22e86a56ace8cca8bfb554913bf06fcb1cefda6d
Ankur Boyed <ankur.boyed@gmail.com>
2025-10-08 20:19:27 -0700

    Add script

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="canonical" href="https://ankurboyed.com/" />
+        <Script
+          src="https://augmentic-client.augmentic.workers.dev/client.js?site=ankur.com"
+          strategy="beforeInteractive" // ensures it runs before React mounts
+        />
       </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
